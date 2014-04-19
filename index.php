388166d1d087bcae8b5f2179661b609e51b73994
MrBlackD <blackpirates@mail.ru>
2014-04-19 23:23:04 +0400

    Added the templates

---
+++ index.php
@@ -0,0 +1,13 @@
+﻿<?php//htmlspecialchars
+	include("connect.php");
+	if(isset($_GET['id']))
+	{
+		$id=$_GET['id'];
+		$desc=mysql_query("SELECT * FROM shop WHERE id='$id'");
+		$res=mysql_fetch_assoc($desc);
+		$site_name=$res['title'];
+	}
+	if(isset($_GET['cat']))
+		$cat=$_GET['cat'];
+   
+?>
